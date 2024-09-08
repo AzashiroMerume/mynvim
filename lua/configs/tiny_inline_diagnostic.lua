2e924ae07bd3968f0ec40ed5db957846cbce196f
AzashiroMerume <sanzhikwantadrive@gmail.com>
2024-09-08 18:39:55 +0500

    removed the hotkey for toggle tid

--- lua/configs/tiny_inline_diagnostic.lua
+++ lua/configs/tiny_inline_diagnostic.lua
@@ -1,10 +1,2 @@
 -- Disable virtual text by default
 vim.diagnostic.config({ virtual_text = false })
-
--- Keymap to toggle inline diagnostics using <leader>d
-vim.keymap.set("n", "<leader>d", function()
-	-- Toggle diagnostics
-	local diagnostics_shown = require("tiny-inline-diagnostic").toggle()
-
-	print("tiny-inline-diagnostic toggled")
-end, { desc = "Toggle Inline Diagnostic" })
