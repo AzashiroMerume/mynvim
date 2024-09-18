58526616f141e653c3ce316a6d39dc99a26577cd
AzashiroMerume <sanzhikwantadrive@gmail.com>
2024-09-18 16:37:51 +0500

    changed keymap for adding to the harpoon

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -1,7 +1,7 @@
 local harpoon = require("harpoon")
 harpoon:setup({})
 
-vim.keymap.set("n", "<leader>a", function()
+vim.keymap.set("n", "<C-a>", function()
     harpoon:list():add()
 end)
 vim.keymap.set("n", "<C-p>", function()
