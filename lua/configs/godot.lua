b4b99afa70f9ba7b6bd77f03411f3393fa773079
AzashiroMerume <sanzhikwantadrive@gmail.com>
2024-11-27 21:11:07 +0500

    start godot lsp connection in case dir is godot project on DirChanged

--- lua/configs/godot.lua
+++ lua/configs/godot.lua
@@ -1,3 +1,4 @@
+-- FileType autocmd for GDScript settings
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "gdscript",
     callback = function()
@@ -9,11 +10,13 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+-- Function to load Godot address
 local function get_godot_address()
     load_dotenv()
     return os.getenv("GODOT_ADDRESS") or "127.0.0.1:55432"
 end
 
+-- Function to check if address is in use
 local function is_address_in_use(address)
     local ip, port = address:match("(.+):(%d+)")
     local command
@@ -28,13 +31,28 @@ local function is_address_in_use(address)
     return result ~= ""
 end
 
-local gdproject = io.open(vim.fn.getcwd() .. "/project.godot", "r")
-if gdproject then
-    io.close(gdproject)
-    local godot_address = get_godot_address()
-    if is_address_in_use(godot_address) then
-        print("Address " .. godot_address .. " is already in use. Aborting connection.")
-        return
+-- Function to start Godot connection if in a Godot project
+local function start_godot_connection()
+    local godot_project_file = vim.fn.getcwd() .. "/project.godot"
+    local gdproject = io.open(godot_project_file, "r")
+    if gdproject then
+        io.close(gdproject)
+        local godot_address = get_godot_address()
+        if is_address_in_use(godot_address) then
+            print("Address " .. godot_address .. " is already in use. Aborting connection.")
+            return
+        end
+        vim.fn.serverstart(godot_address)
+        print("Godot connection started at " .. godot_address)
     end
-    vim.fn.serverstart(godot_address)
 end
+
+-- Run connection check on startup
+start_godot_connection()
+
+-- Autocmd for when the working directory changes
+vim.api.nvim_create_autocmd("DirChanged", {
+    callback = function()
+        start_godot_connection()
+    end,
+})
