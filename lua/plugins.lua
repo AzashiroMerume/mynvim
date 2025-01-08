faaf34efb743a9b3d43a95cb86465c7ee442c0a4
AzashiroMerume <sanzhikwantadrive@gmail.com>
2025-01-08 23:26:36 +0500

    change blink cmp setup for updated LuaSnip; updated plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -208,7 +208,7 @@ require("lazy").setup({
     -- New Completion Setup (blink-cmp)
     {
         "saghen/blink.cmp",
-        dependencies = { "rafamadriz/friendly-snippets", { "L3MON4D3/LuaSnip", version = "v2.*" } },
+        dependencies = { "rafamadriz/friendly-snippets", { "L3MON4D3/LuaSnip", version = "v2.*," } },
         event = "VeryLazy",
         version = "v0.*",
 
@@ -221,22 +221,13 @@ require("lazy").setup({
             },
 
             signature = { enabled = true },
-            snippets = {
-                expand = function(snippet)
-                    require("luasnip").lsp_expand(snippet)
-                end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require("luasnip").jumpable(filter.direction)
-                    end
-                    return require("luasnip").in_snippet()
-                end,
-                jump = function(direction)
-                    require("luasnip").jump(direction)
-                end,
-            },
+
             sources = {
-                default = { "lsp", "path", "luasnip", "buffer" },
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+
+            snippets = {
+                preset = "luasnip",
             },
         },
     },
